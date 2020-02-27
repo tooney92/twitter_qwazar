a988d4bfe5f2c69b0033b0c78541020ec20fc07e
tooney92 <anthony.ime@outsourceglobal.com>
2020-02-27 19:57:00 +0100

    fixed the layout issue.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in?, :only => [:show, :edit, :destroy, :update]
+  before_action :logged_in?, :only => [:edit, :destroy, :update]
 
   def new
   end
