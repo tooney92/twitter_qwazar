18aa374d3e244b441c4182c417d27bb23a2b679b
amirdbt <ahmed.dambatta@outsourceglobal.com>
2020-03-02 17:21:42 +0100

    chhange

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,5 +2,3 @@
 # if ENV["REDIS_PROVIDER"]
 #     $redis = Redis.new(:url => ENV["REDIS_PROVIDER"])
 # end
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
