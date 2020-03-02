99a3eb412fe2a93dee76de59e0cf29176f035f72
amirdbt <ahmed.dambatta@outsourceglobal.com>
2020-03-02 17:11:21 +0100

    chhange

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
 #$redis = Redis::Namespace.new("twitter_clone", :redis => Redis.new)
-if ENV["REDIS_PROVIDER"]
-    $redis = Redis.new(:url => ENV["REDIS_PROVIDER"])
-end
+# if ENV["REDIS_PROVIDER"]
+#     $redis = Redis.new(:url => ENV["REDIS_PROVIDER"])
+# end
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
