a841ef59f101af98f79ffc3598566a83c61af200
amirdbt <ahmed.dambatta@outsourceglobal.com>
2020-03-03 09:50:34 +0100

    commt

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,4 +4,4 @@
 # end
 ENV["REDISTOGO_URL"] = 'redis://redistogo:4672deadc8b11656fcb0e0fad8c06c52@pike.redistogo.com:10427/'
 uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
